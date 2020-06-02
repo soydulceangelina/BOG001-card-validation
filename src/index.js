551b7f42f7aa723c8fbacba4cfecffa82733b99d
Paola Sarmiento <paolasarmiento@pop-os.localdomain>
2020-06-02 10:01:05 -0500

    cambios para merge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
 console.log(validator);
+
+const formValidator = document.getElementById("form"); // form
+const userCreditCard = document.getElementById("numberTdc"); // input
+
+formValidator.addEventListener("submit", getNumber);
+
+function getNumber(event) {
+  event.preventDefault();
+  const tdcNumber = userCreditCard.value; // value
+  validator.isValid(tdcNumber);
+}
--- src/validator.js
+++ src/validator.js
@@ -1,10 +1,33 @@
 const validator = {
+  isValid: function (num) {
+    let numsGroup = num.split("").reverse();
+    let sum = 0;
+
+    for (let i = 1; i < numsGroup.length; i += 2) {
+      let multi = numsGroup[i] * 2;
+      if (multi >= 10) {
+        multi = multi.toString();
+        multi = multi.split("");
+        multi = parseInt(multi[0]) + parseInt(multi[1]);
+      }
+      numsGroup[i] = multi;
+    }
+
+    for (let j = 0; j < numsGroup.length; j++) {
+      let odd = numsGroup[j];
+      odd = parseInt(odd);
+      numsGroup[j] = odd;
+    }
+    numsGroup.forEach(function (digit) {
+      sum += digit;
+    });
+  },
   /*
   cuando ingresa el número ya validado lo retorna como un string cifrado con # exceptuando los últimos 4 digitos
   params: creditCardNumber - string
   return: tarjeta de credito cifrada - string
   */
-  maskify(creditCardNumber) {
+  maskify: function (creditCardNumber) {
     let creditCardNumberReversed = "";
     for (let i = creditCardNumber.length - 1; i >= 0; i--) {
       creditCardNumberReversed = creditCardNumberReversed + creditCardNumber[i];
