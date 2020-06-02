af40e41c497eb1689089b789081e628c5d4de7fe
Dulce Mercado <soydulceangelina@gmail.com>
2020-06-02 00:45:52 -0500

    validator logic

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import validator from './validator.js';
 
 console.log(validator);
 
-const formValidator = document.getElementById('form');
-const userCreditCard = document.getElementById('numberTdc');
+const formValidator = document.getElementById('form'); // form
+const userCreditCard = document.getElementById('numberTdc'); // input
 
 formValidator.addEventListener('submit', getNumber);
 
 
 function getNumber(event){
     event.preventDefault();
-    const tdcNumber = userCreditCard.value;
-    console.log(tdcNumber);
+    const tdcNumber = userCreditCard.value; // value
+    validator.isValid(tdcNumber);
 }
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,29 @@
 const validator = {
-  // ...
+  isValid: function(num){
+    let numsGroup = num.split("").reverse();
+    let sum = 0;
+
+    for(let i = 1; i < numsGroup.length; i += 2){
+      let multi = numsGroup[i] * 2;
+      if(multi >= 10){
+        multi = multi.toString();
+        multi = multi.split("");
+        multi = parseInt(multi[0]) + parseInt(multi[1]);
+      }
+      numsGroup[i] = multi;
+    }
+
+    for(let j = 0; j < numsGroup.length; j++){
+      let odd = numsGroup[j];
+      odd = parseInt(odd);
+      numsGroup[j] = odd;
+    }
+    numsGroup.forEach(function(digit) {
+      sum += digit
+    });
+
+    console.log(sum);
+  }
 };
 
 export default validator;
