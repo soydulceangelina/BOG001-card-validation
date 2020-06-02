758409501013c6f1f7bda8d3d20db5b1163c2be9
Dulce Mercado <soydulceangelina@gmail.com>
2020-06-01 22:33:16 -0500

    get number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const formValidator = document.getElementById('form');
+const userCreditCard = document.getElementById('numberTdc');
+
+formValidator.addEventListener('submit', getNumber);
+
+
+function getNumber(event){
+    event.preventDefault();
+    const tdcNumber = userCreditCard.value;
+    console.log(tdcNumber);
+}
