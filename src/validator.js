7b6a13b58f34e0633ee9aa03df448068edd9ee24
Paola Sarmiento <paolasarmiento@pop-os.localdomain>
2020-06-01 22:16:38 -0500

    Prueba Maskify

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,29 @@
 const validator = {
-  // ...
+  /*
+  cuando ingresa el número ya validado lo retorna como un string cifrado con # exceptuando los últimos 4 digitos
+  params: creditCardNumber - string
+  return: tarjeta de credito cifrada - string
+  */
+  maskify(creditCardNumber) {
+    let creditCardNumberReversed = "";
+    for (let i = creditCardNumber.length - 1; i >= 0; i--) {
+      creditCardNumberReversed = creditCardNumberReversed + creditCardNumber[i];
+    }
+
+    let maskReversed = "";
+    for (let i = 0; i < creditCardNumberReversed.length; i++) {
+      if (i < 4) {
+        maskReversed = maskReversed + creditCardNumberReversed[i];
+      } else {
+        maskReversed = maskReversed + "#";
+      }
+    }
+    let mask = "";
+    for (let i = maskReversed.length - 1; i >= 0; i--) {
+      mask = mask + maskReversed[i];
+    }
+    return mask;
+  },
 };
 
 export default validator;
--- test/validator.spec.js
+++ test/validator.spec.js
@@ -1,14 +1,14 @@
 // importamos el objeto `validator`, que contiene las funciones `isValid` y `maskify`
-import validator from '../src/validator';
+import validator from "../src/validator";
 
-describe('validator', () => {
-  it('debería ser un objeto', () => {
-    expect(typeof validator).toBe('object');
+describe("validator", () => {
+  it("debería ser un objeto", () => {
+    expect(typeof validator).toBe("object");
   });
 
-  describe('validator.isValid', () => {
-    it('debería ser una función', () => {
-      expect(typeof validator.isValid).toBe('function');
+  describe("validator.isValid", () => {
+    it("debería ser una función", () => {
+      expect(typeof validator.isValid).toBe("function");
     });
 
     it.skip('debería retornar true para "4083952015263"', () => {
@@ -24,21 +24,21 @@ describe('validator', () => {
     });
   });
 
-  describe('validator.maskify', () => {
-    it('debería ser una función', () => {
-      expect(typeof validator.maskify).toBe('function');
+  describe("validator.maskify", () => {
+    it("debería ser una función", () => {
+      expect(typeof validator.maskify).toBe("function");
     });
 
-    it.skip('Debería retornar "############5616" para "4556364607935616"', () => {
-      // escribe aquí tu test
+    it('Debería retornar "############5616" para "4556364607935616"', () => {
+      expect(validator.maskify("4556364607935616")).toBe("############5616");
     });
 
-    it.skip('Debería retornar "1" para "1"', () => {
-      // escribe aquí tu test
+    it('Debería retornar "1" para "1"', () => {
+      expect(validator.maskify("1")).toBe("1");
     });
 
-    it.skip('Debería retornar "######orld" para "helloworld"', () => {
-      // escribe aquí tu test
+    it('Debería retornar "######orld" para "helloworld"', () => {
+      expect(validator.maskify("helloworld")).toBe("######orld");
     });
   });
 });
