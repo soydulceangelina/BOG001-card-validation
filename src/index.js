6a270ec8efe509d3c10e8a6f5519714bfa0b7c09
Dulce Mercado <soydulceangelina@gmail.com>
2020-06-03 12:01:33 -0500

    validation layout

--- src/index.js
+++ src/index.js
@@ -3,24 +3,26 @@ import validator from './validator.js';
 const formValidator = document.getElementById('form'); // form
 const userCreditCard = document.getElementById('numberTdc'); // input
 const invalidCreditCard = document.getElementById('invalidCard');
-const validCreditCard = document.getElementById('validCard').style.display = "none";
+const validCreditCard = document.getElementById('validCard');
+const validCardReport = document.getElementById('validCardMessage');
 
 formValidator.addEventListener('submit', getNumber);
-
+validCreditCard.style.display = "none";
 
  function approvedTransaction(){
+    const tdcNumber = userCreditCard.value;
+    const maskify = validator.maskify(tdcNumber);
     document.getElementById('validCard').style.display = "block";
     document.getElementById('form').style.display = "none";
-}
-
+    validCardReport.innerHTML = `La transaccion con la tarjeta ${maskify} ha sido exitosa`
+ }
 function getNumber(event){
     event.preventDefault();
-    const tdcNumber = userCreditCard.value; // value
+    const tdcNumber = userCreditCard.value;
     if (validator.isValid(tdcNumber) === true){
        approvedTransaction();
     }else {
         invalidCreditCard.innerHTML = 'Tarjeta no valida';
+        setTimeout(function(){ location.reload(); }, 1000);
     }
-    validator.maskify(tdcNumber)
-
 }
