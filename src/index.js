f48fbf2d156bc4444d9b2cfe19d04d6f9c9cace2
Dulce Mercado <soydulceangelina@gmail.com>
2020-06-05 17:05:14 -0500

    new view

--- src/index.js
+++ src/index.js
@@ -5,11 +5,22 @@ const userCreditCard = document.getElementById('numberTdc');
 const invalidCreditCard = document.getElementById('invalidCard');
 const validCreditCard = document.getElementById('validCard');
 const validCardReport = document.getElementById('validCardMessage');
+// const selectionView = document.getElementById('firstView');
+const affiliateButton = document.getElementById('affiliatebutton');
+
 
 formValidator.addEventListener('submit', getNumber);
+affiliateButton.addEventListener('click', secondView);
 validCreditCard.style.display = "none";
+formValidator.style.display = "none";
 
- function approvedTransaction(){
+function secondView(){
+  document.getElementById('firstView').style.display = "none";
+  document.getElementById('validCard').style.display = "none"
+  document.getElementById('form').style.display = "block";
+}
+
+function approvedTransaction(){
     const tdcNumber = userCreditCard.value;
     const maskify = validator.maskify(tdcNumber);
     document.getElementById('validCard').style.display = "block";
@@ -23,16 +34,17 @@ function getNumber(event){
        approvedTransaction();
     }else {
         invalidCreditCard.innerHTML = 'Tarjeta no valida';
-        setTimeout(function(){ location.reload(); }, 1000);
+        // setTimeout(function(){ location.reload(); }, 1000);
     }
 }
 
-document.getElementById("numberTdc").addEventListener("input", onlyNumber);
+userCreditCard.addEventListener("input", onlyNumber);
 
 function onlyNumber() {
   let numberTdc = document.getElementById("numberTdc");
-  const regEx = new RegExp(/^\d+$/);
-  if (!regEx.test(numberTdc.value)) {
-    numberTdc.value = numberTdc.value.substring(0, numberTdc.value.length - 1);
-  }
+  const regEx = new RegExp(/\D/g);
+  numberTdc.value = numberTdc.value.replace(regEx,'');
+  // if (!regEx.test(numberTdc.value)) { 
+  //   numberTdc.value = numberTdc.value.substring(0, numberTdc.value.length - 1);
+  // }
 }
